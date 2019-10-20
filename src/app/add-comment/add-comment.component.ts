e2de0ce662e2cce091af243ca201e1df58f71ebf
Rodrigo Araújo Marinho Franco <contact@rodrigoamf.com>
2019-10-20 12:07:17 -0300

    #23 Add-comment component

--- src/app/add-comment/add-comment.component.ts
+++ src/app/add-comment/add-comment.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-add-comment',
+  selector: 'add-comment',
   templateUrl: './add-comment.component.html',
   styleUrls: ['./add-comment.component.scss']
 })
 export class AddCommentComponent implements OnInit {
+  @Input() author: String;
+
+  public clearTextArea(event){
+    let text: String = event.currentTarget.value.replace(/\s/g, '');
+    if(text == 'Writearesponse...'){
+      event.currentTarget.value = '';
+    }
+  }
+
+  public leftTextArea(event){
+    let text: String = event.currentTarget.value.replace(/\s/g, '');
+    if(text == ''){
+      event.currentTarget.value = "Write a response...";
+    }
+  }
 
   constructor() { }
 
