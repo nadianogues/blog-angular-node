6b2e625ef36b10d42a2ce3e6c7c863b4417becd7
Rodrigo Araújo Marinho Franco <contact@rodrigoamf.com>
2019-10-19 21:27:23 -0300

    #3 Add post component

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'post',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
+  @Input() author: string;
+  @Input() date: string;
+  @Input() title: string;
+  @Input() comments: string;
+  randomValue: string;
+
+  getImageSrc(){
+    return "https://picsum.photos/1024/777/?random=" + this.randomValue;
+  }
+
   constructor() { }
 
   ngOnInit() {
+    // Value definied here to avoid the error ExpressionChangedAfterItHasBeenCheckedError
+    this.randomValue = Math.floor((Math.random() * 100) + 1).toString();
   }
 
 }
