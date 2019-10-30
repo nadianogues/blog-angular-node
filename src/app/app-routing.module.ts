47947b95e724ec2153e4bbdd7c158f4f6effb10c
Rodrigo Araújo Marinho Franco <contact@rodrigoamf.com>
2019-10-30 19:03:28 -0300

    #14 fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostPageComponent } from './post-page/post-page.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = 
 [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'post',
     component: PostPageComponent
